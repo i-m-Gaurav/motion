06a825e35b26d1b4b21612b3146cb38f098a6cc2
Gaurav <indiagauravkumar@gmail.com>
2025-11-11 18:49:28 +0530

    Loader on the sign in button.

--- app/components/sign-in.tsx
+++ app/components/sign-in.tsx
@@ -1,8 +1,27 @@
+"use client";
+import React from "react";
 
-"use client"
+import { signIn } from "next-auth/react";
 
-import { signIn } from "next-auth/react"
- 
 export default function SignIn() {
-  return <button className="cursor-pointer" onClick={() => {signIn("google")}}>Login with Google</button>
+  const [loading, setLoading] = React.useState(false);
+  return (
+    <button
+      className="cursor-pointer bg-neutral-800 py-2 flex items-center justify-center w-full mx-auto py-1 rounded"
+      onClick={ async () => {
+        try {
+          setLoading(true);
+          await signIn("google");
+          
+        } catch (error) {
+          setLoading(false);
+          console.error("Error during sign-in:", error);
+        }
+        
+      }}
+    >
+      {loading ?   <div className="w-4 h-4 border-gray-400 border-2 border-t-transparent rounded-full animate-spin"></div>
+ : <p className="text-sm">Login With Google</p>}
+    </button>
+  );
 }
